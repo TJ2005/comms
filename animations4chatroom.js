006194899e755d5f52b4f56b501cbe5c657bb4a7
Dhruv Rathod <128411388+spicy-potato-cat@users.noreply.github.com>
2024-09-22 01:43:09 +0530

    .

--- animations4chatroom.js
+++ animations4chatroom.js
@@ -1,1 +1,14 @@
-// Will work on it later
+console.log("Hello");
+
+
+function showsendopts() {
+  var sendopts = document.getElementById("sendopts");
+  if (sendopts.style.display === "none"){
+    sendopts.style.display = "flex";
+    sendopts.style.animation= 'pullUp 0.5s forwards';
+  }else{
+  var sendopts = document.getElementById("sendopts");
+  sendopts.style.animation= 'pullDown 0.5s forwards';
+  sendopts.style.display = "none";
+  }
+}
